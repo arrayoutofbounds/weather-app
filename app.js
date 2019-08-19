d5fc14f9550c4a68e45ef5836ce50942f9b83c36
Anmol Desai <anmoldesai4@gmail.com>
2019-08-19 12:31:48 +1000

    added geocoding

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const request = require('request');
 
 const URL =
-  'https://api.darksky.net/forecast/f6807345f31c2edde3f429275a44447e/37.8267,-122.4233';
+    'https://api.darksky.net/forecast/f6807345f31c2edde3f429275a44447e/37.8267,-122.4233?units=si';
 
 request(
   {
-    url: URL
+    url: URL,
+    json: true
   },
   (error, response) => {
-    const r = JSON.parse(response.body);
-    console.log(r);
+    if(error){
+        console.log("Unable to connect to weather service");
+    }else{
+        const temp = response.body.currently.temperature;
+    const precipProbability = response.body.currently.precipProbability;
+    console.log(response.body.daily.data[0].summary + `It is currently ${temp}. There is a ${precipProbability} chance of rain`);
+    }
   }
 );
+
+//geocoding
+// const geocodingUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW5tb2xkZXNhaTQiLCJhIjoiY2p6ZzU1cTVrMGdsZTNicDVla200YTk0aCJ9.NxfMJQIBGMIEnMBmcHZ9iA";
+
+// request(
+//     {
+//         url: geocodingUrl,
+//         json: true
+//     },
+//     (error, response) => {
+//         console.log("longtitude", response.body.features[0].center[0], "latitude", response.body.features[0].center[1]);
+//     });
