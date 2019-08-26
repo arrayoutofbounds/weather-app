07c07a7c6121bed4d2cc79de43ba4b9045da9317
Anmol Desai <anmoldesai4@gmail.com>
2019-08-26 20:30:58 +1000

    get weather for a location

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const geoCode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 // location -> longtitude, latitude -> get weather
+// callbacks have two arguments usually. first is error, second is the response.
 
-geoCode("Sydney", (error, data) => {
-    if(error){
-        console.log(error);
-    }else{
-        forecast( data.lattitude, data.longtitude, (error, data) => {
-            if(error){
-                console.log(error);
-            }else{
-                console.log(data);
+const locationToGet = process.argv[2];
+
+if(locationToGet){
+    geoCode(locationToGet.toString(), (error, data) => {
+        if (error) {
+            return console.log(error);
+        }
+    
+        forecast(data.lattitude, data.longtitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error);
             }
+    
+            console.log(`Location is ${data.location} is ${forecastData.temp} degrees celcius and prepcipitation is ${forecastData.precipProbability}. Overall it is ${forecastData.summary}.`);
         })
-    }
-});
+    });
+}else{
+    console.log("Please provide an input");
+}
